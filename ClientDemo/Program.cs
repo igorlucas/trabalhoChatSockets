c7c4d859950aa9ad8413dcca33b725b01d09ee9b
Igor Silva <igorlucas.ti@outlook.com>
2018-09-06 20:25:53 -0300

    implementado comunicação com tcp e udp

--- ClientDemo/Program.cs
+++ ClientDemo/Program.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace ClientDemo
 {
@@ -13,10 +10,53 @@ namespace ClientDemo
     {
         static void Main(string[] args)
         {
+            RunUdpClient();
+            Process.GetCurrentProcess().WaitForExit();
+        }
+
+        
+        private static void RunUdpClient()
+        {
+            Console.Title = "Cliente UDP";
+            //Criando um cliente UDP
+            string clientIp = "127.0.0.10";
+            int clientPort = 2001;
+            IPEndPoint connectionClient = new IPEndPoint(IPAddress.Parse(clientIp), clientPort);
+            UdpClient socketClientUdp = new UdpClient(connectionClient);
+
+            //Conexão do host que será enviado a mensagem
+            string remoteIp = "127.0.0.1";
+            int remotePort = 2000;
+            IPEndPoint connectionServer = new IPEndPoint(IPAddress.Parse(remoteIp), remotePort);
+
+            //váriavel que recebera os dados digitados
+            string dados = "";
+            byte[] mensagemEnviada;
+
+            try
+            {
+                //Estabelece conexão de um cliente com um servidor
+                socketClientUdp.Connect(connectionServer);
+                while (dados.ToLower() != "sair")//condição de saída do canal
+                {
+                    Console.WriteLine("Digite alguma coisa!");
+                    dados = Console.ReadLine();
+                    mensagemEnviada = Encoding.ASCII.GetBytes(dados);
+                    socketClientUdp.Send(mensagemEnviada, mensagemEnviada.Length);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        private static void RunTcpClient()
+        {
             Console.Title = "Cliente";
-            Socket listen = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            IPEndPoint connect = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2000);
-            listen.Connect(connect);
+            Socket socketClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint connectionServer = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2000);
+            socketClient.Connect(connectionServer);
 
             string dados = "";
             while (dados.ToLower() != "sair")
@@ -25,14 +65,8 @@ namespace ClientDemo
                 dados = Console.ReadLine();
                 byte[] mensagemEnviada = new byte[100];
                 mensagemEnviada = Encoding.Default.GetBytes(dados);
-                listen.Send(mensagemEnviada);
+                socketClient.Send(mensagemEnviada);
             }
-
-
-
-
-            //Console.ReadKey();
-            Process.GetCurrentProcess().WaitForExit();
         }
     }
 }
--- ServerDemo/Program.cs
+++ ServerDemo/Program.cs
@@ -13,34 +13,68 @@ namespace ServerDemo
     {
         static void Main(string[] args)
         {
+            RunUdpServer();
+            Process.GetCurrentProcess().WaitForExit();
+        }
+
+        private static void RunUdpServer()
+        {
+            Console.Title = "SERVIDOR UDP";
+            //Criando um servidor UDP
+            string serverIp = "127.0.0.1";
+            int serverPort = 2000;
+            IPEndPoint connectionServer = new IPEndPoint(IPAddress.Parse(serverIp), serverPort);
+            UdpClient socketServerUdp = new UdpClient(connectionServer);
+
+
+            //Exibindo informações
+            Console.WriteLine($"Servidor UDP escutando no endereço: {serverIp}:{serverPort}");
+            Console.WriteLine("-------------------------------------------------------------\n");
+
+            //Inicia recebimento assincrono de dados
+            socketServerUdp.BeginReceive(DataReceived, socketServerUdp);
+        }
+
+        private static void RunTcpServer()
+        {
+            //Criando um host servidor
             Console.Title = "Servidor";
-            Socket listen = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            Socket conexao;
+            Socket connectionServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Socket connectionRemote;
             IPEndPoint ip_porta = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2000);
 
-            listen.Bind(ip_porta);
-            listen.Listen(10);
+            //Lingando o socket servidor a um endereço porta ip
+            connectionServer.Bind(ip_porta);
+            //Definindo o numero máximo de conexões
+            connectionServer.Listen(10);
             Console.WriteLine("Servidor escutando");
 
-            conexao = listen.Accept();
-            Console.WriteLine("Conexão aceita");
+            connectionRemote = connectionServer.Accept();
+            Console.WriteLine("Um novo usuário acaba de entrar");
 
-
-            
             int array_size = 0;
             byte[] mensagemRecebida = new byte[1000];
-
-            while (conexao.Connected)
+            array_size = connectionRemote.Receive(mensagemRecebida, 0, mensagemRecebida.Length, 0);
+            while (connectionRemote.Connected)
             {
-                array_size = conexao.Receive(mensagemRecebida, 0, mensagemRecebida.Length, 0);
-                Array.Resize(ref mensagemRecebida, array_size);
                 var dados = Encoding.Default.GetString(mensagemRecebida);
                 Console.WriteLine("A informação recebida é: {0}", dados);
             }
-
-
-            //Console.ReadKey();
-            Process.GetCurrentProcess().WaitForExit();
         }
+
+        private static void DataReceived(IAsyncResult result)
+        {
+            UdpClient socketClientUdp = (UdpClient)result.AsyncState;
+            IPEndPoint connectionClient = new IPEndPoint(IPAddress.Any, 0);
+            byte[] mensagemRecebida = socketClientUdp.EndReceive(result, ref connectionClient);
+
+            //Converte o array de buffer em string  
+            string dados = ASCIIEncoding.ASCII.GetString(mensagemRecebida);
+            Console.Write(connectionClient + " disse: " + dados + Environment.NewLine);
+
+            //Reinicia recebimento de dados
+            socketClientUdp.BeginReceive(DataReceived, result.AsyncState);
+        }
+
     }
 }
