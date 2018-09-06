95a8717a877f7f2cc94a5e620e12f31d63734fee
Igor Silva <igorlucas.ti@outlook.com>
2018-09-06 02:39:04 -0300

    enviando projeto

---
+++ ClientDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Cliente";
+            Socket listen = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint connect = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2000);
+            listen.Connect(connect);
+
+            string dados = "";
+            while (dados.ToLower() != "sair")
+            {
+                Console.WriteLine("Digite alguma coisa!");
+                dados = Console.ReadLine();
+                byte[] mensagemEnviada = new byte[100];
+                mensagemEnviada = Encoding.Default.GetBytes(dados);
+                listen.Send(mensagemEnviada);
+            }
+
+
+
+
+            //Console.ReadKey();
+            Process.GetCurrentProcess().WaitForExit();
+        }
+    }
+}
---
+++ ServerDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Servidor";
+            Socket listen = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Socket conexao;
+            IPEndPoint ip_porta = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2000);
+
+            listen.Bind(ip_porta);
+            listen.Listen(10);
+            Console.WriteLine("Servidor escutando");
+
+            conexao = listen.Accept();
+            Console.WriteLine("Conexão aceita");
+
+
+            
+            int array_size = 0;
+            byte[] mensagemRecebida = new byte[1000];
+
+            while (conexao.Connected)
+            {
+                array_size = conexao.Receive(mensagemRecebida, 0, mensagemRecebida.Length, 0);
+                Array.Resize(ref mensagemRecebida, array_size);
+                var dados = Encoding.Default.GetString(mensagemRecebida);
+                Console.WriteLine("A informação recebida é: {0}", dados);
+            }
+
+
+            //Console.ReadKey();
+            Process.GetCurrentProcess().WaitForExit();
+        }
+    }
+}
